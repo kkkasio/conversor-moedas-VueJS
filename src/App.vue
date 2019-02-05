67c98d81288e1b3ddcf6a3b88dd0134cde58bf26
Kasio Eduardo <kasioeduardo13@gmail.com>
2019-02-05 12:15:39 -0200

    utilizado uma simples api

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" class="text-center">
+    <h1 class="text-center">Conversor  de Moedas</h1>
+
+    <div class="linha">
+      <Conversor moeda-a="USD" moeda-b="BRL"></Conversor>
+      <Conversor moeda-a="BRL" moeda-b="USD"></Conversor>
+    </div>
+
+    <div class="linha">
+        <Conversor moeda-a="CAD" moeda-b="BRL"></Conversor>
+      <Conversor moeda-a="BRL" moeda-b="CAD"></Conversor>
+    </div>
+
+    <div class="linha">
+      <Conversor moeda-a="EUR" moeda-b="BRL"></Conversor>
+      <Conversor moeda-a="BRL" moeda-b="EUR"></Conversor>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+
+import Conversor from "./components/Conversor";
+export default {
+  name: 'app',
+  components: {
+    Conversor
+  }
+}
+</script>
+
+<style>
+#app {
+  max-width: 900px;
+  margin: auto;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 30px;
+}
+  .linha{
+    margin-top: 40px;
+    display: flex;
+    justify-content: space-around;
+
+  }
+
+</style>
---
+++ src/components/Conversor.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="conversor">
+
+        <h2>{{moedaA}} para {{moedaB}}</h2>
+        <input type="text"  v-model="moedaA_value" :placeholder="moedaA">
+        <input type="button" value="Converter"  v-on:click="converter">
+        <h2>{{moedaB_value}}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name : "Conversor",
+        props: ["moedaA","moedaB"],
+
+        data(){
+            return {
+                moedaA_value: "",
+                moedaB_value: 0
+            };
+        },
+
+        methods:{
+            converter() {
+                let de_para = this.moedaA + "_" + this.moedaB;
+                let url = "http://free.currencyconverterapi.com/api/v5/convert?q=" + de_para + "&compact=y";
+
+                fetch(url).then(res => {
+                    return res.json();
+                }).then(json => {
+                        let cotacao = json[de_para].val;
+                        this.moedaB_value = (cotacao * parseFloat(this.moedaA_value)).toFixed(2);
+                    })
+            }
+        }
+    };
+
+</script>
+
+<style scoped>
+    .conversor{
+        max-width: 300px;
+        padding: 20px;
+        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    }
+
+</style>
